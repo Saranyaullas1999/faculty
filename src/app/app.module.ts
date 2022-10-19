6240d392d6cf549968360aa2650cbeaed7351ef3
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-20 03:04:47 +0530

    aPI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AddfacultyComponent } from './addfaculty/addfaculty.component';
 import { RouterModule,Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ViewfacultyComponent } from './viewfaculty/viewfaculty.component';
-
+import{HttpClientModule} from '@angular/common/http'
 
 const appRoutes:Routes=[
   {
@@ -29,7 +29,8 @@ const appRoutes:Routes=[
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(appRoutes),
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
