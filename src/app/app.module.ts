906976ea0edf21859a7c19fa94a08fff56763d77
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-20 02:16:30 +0530

    addfaculty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AddfacultyComponent } from './addfaculty/addfaculty.component';
+import { RouterModule,Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+
+const appRoutes:Routes=[
+  {
+    path:"",component:AddfacultyComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddfacultyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
